c0b6758c5fd03cff67ef4bcee9ef50628c04f9d5
Zombie <chuanghs@gmail.com>
2013-04-26 11:45:51 +0800

    rename file name to comply with class name

--- lib/poi/document.rb
+++ lib/poi/document.rb
@@ -6,5 +6,5 @@ require 'java'
 require File.join(JRUBY_POI_LIB_PATH, 'poi', 'document', 'document')
 require File.join(JRUBY_POI_LIB_PATH, 'poi', 'document', 'paragraph')
 require File.join(JRUBY_POI_LIB_PATH, 'poi', 'document', 'table')
-require File.join(JRUBY_POI_LIB_PATH, 'poi', 'document', 'row')
-require File.join(JRUBY_POI_LIB_PATH, 'poi', 'document', 'cell')
+require File.join(JRUBY_POI_LIB_PATH, 'poi', 'document', 'table_row')
+require File.join(JRUBY_POI_LIB_PATH, 'poi', 'document', 'table_cell')
